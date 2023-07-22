81d4d9f8ec5d2db3bde10b4bbac99f955c1d29e5
bhargavratnala <bhargavratnala2004@gmail.com>
2023-07-23 03:11:46 +0530

    update theme

--- asserts/script.js
+++ asserts/script.js
@@ -1,40 +1,47 @@
-let more = document.getElementById("more");
-let line1 = document.getElementById("line1");
-let line2 = document.getElementById("line2");
-let line3 = document.getElementById("line3");
-let navList = document.getElementById("navList");
-more.addEventListener("click", ()=>{
-    line1.classList.toggle("cross");
-    line2.classList.toggle("cross");
-    line3.classList.toggle("cross");
-    navList.classList.toggle("nav-visible");
-});
-function openlive(url){
-    let frame = document.getElementById("pro-frame");
-    let framebut = document.getElementById("proframeclosebut");
-    frame.classList.remove("close");
-    framebut.classList.remove("close");
-    frame.setAttribute("src", url);
-}
-function closeproframe(){
-    let frame = document.getElementById("pro-frame");
-    let framebut = document.getElementById("proframeclosebut");
-    frame.classList.add("close");
-    framebut.classList.add("close");
-}
+window.addEventListener("scroll", scrollHandler);
+let sections = document.getElementsByTagName("section");
+let navItems = document.getElementsByClassName("nav-item");
+let navItemscreen = document.getElementsByClassName("nav-item-screen");
+let nav =document.getElementById("nav");
+let navbut = document.getElementById("nav-but");
+let active = 0;
 
-const observer = new IntersectionObserver(entry =>{
-    console.log(entry)
-    entry.forEach(e=>{
-        console.log(e)
-        if(e.isIntersecting){
-            e.target.classList.add("show-project-card");
+function scrollHandler(){
+    let scrollHeight = window.scrollY;
+    // console.log("scroll height : " + scrollHeight);
+    active = 0;
+    for(let i=0; i<sections.length; i++){
+        let height = sections[i].offsetHeight;
+        let offset = sections[i].offsetTop;
+        // console.log("-------------------");
+        // console.log(offset);
+        // console.log(height);
+        if(offset <= (scrollHeight+100) && scrollHeight < offset+height && active != 1){
+            navItems[i].classList.add("active");
+            navItemscreen[i].classList.add("active");
+            active = 1;
+            if(i != 0){
+                nav.classList.add("nav-background");
+            }
+            else{
+                nav.classList.remove("nav-background");
+            }
         }
         else{
-            e.target.classList.remove("show-project-card");
+            navItems[i].classList.remove("active");
+            navItemscreen[i].classList.remove("active");
         }
-    })
-})
-const elements = document.getElementsByClassName("project-card");
-for(i=0; i<elements.length; i++)
-observer.observe(elements[i]);
+    }
+}
+
+navbut.addEventListener("click", closenav);
+
+function closenav(){
+    let navbarscreen = document.getElementById("nav-bar-screen");
+    navbarscreen.classList.toggle("close-nav");
+}
+
+function toggleData(data){
+    let resumeData = document.getElementById("resume-data");
+    resumeData.className = 'resume-data ' + data;
+}
